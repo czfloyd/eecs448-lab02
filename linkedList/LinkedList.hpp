c81aa707383b37bc4fe64a4d0ad44a657d80e841
czfloyd <floyd.c23@gmail.com>
2019-02-12 22:26:18 -0600

    Added search function

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -44,9 +44,14 @@ bool LinkedList<T>::search(T value) const
 	Node<T>* temp = m_front;
 	bool isFound = false;
 
-	/** TODO 
-		Fix this method
-	*/
+	while (!isFound && temp != nullptr){
+		if (temp->getValue() == value){
+			isFound = true;
+		}
+		else {
+			temp = temp->getNext();
+		}
+	}
 
 	return(isFound);
 }
